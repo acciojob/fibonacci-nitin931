62cfd5108fe19fd15ed32f70a396c20b6e9a3293
nitin931 <55184719+nitin931@users.noreply.github.com>
2024-03-18 19:06:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function fibonacci(num) {
-	if(num === 0)return 0;
-	if(num === 1 )return 1;
+	if(num === 1)return 0;
+	if(num === 2 )return 1;
 	let a = 0;
 	let b = 1;
 	let sum = 0;
-	for(let i = 2; i<= num; i++){
+	for(let i = 2; i< num; i++){
 		sum = a + b;
 		a = b;
 		b = sum;
