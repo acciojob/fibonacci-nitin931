4db19d9de4d8778bd76be65caf16ecaa4723bc02
nitin931 <55184719+nitin931@users.noreply.github.com>
2024-03-18 18:32:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
-// your code here
+	if(num === 0)return 0;
+	if(num === 1 )return 1;
+	let a = 0;
+	let b = 1;
+	let sum = 0;
+	for(let i = 2; i<= num; i++){
+		sum = a + b;
+		a = b;
+		b = sum;
+	}
+	return sum;
 }
 
 module.exports = fibonacci;
